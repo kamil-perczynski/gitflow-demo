ddae24bc6370d5c260a7eb24ee37c1149c401a80
Kamil Perczynski <Kamil.Perczynski@raiffeisen.al>
2023-03-16 21:50:42 +0100

    Add endpoint for reading game rounds

---
+++ src/main/java/perczynski/kamil/evolution/gameservice/domain/rounds/GameRoundService.java
@@ -0,0 +1,28 @@
+package perczynski.kamil.evolution.gameservice.domain.rounds;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import perczynski.kamil.evolution.gameservice.domain.Player;
+import perczynski.kamil.evolution.gameservice.domain.PlayerRepository;
+import perczynski.kamil.evolution.gameservice.domain.bets.GameRound;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class GameRoundService {
+
+    private final PlayerRepository playerRepository;
+    private final GameRoundRepository gameRoundRepository;
+
+    public GameRoundListing listGameRounds(String playerId) {
+        log.info("Listing game rounds for player: {}", playerId);
+        final Player player = playerRepository.find(playerId);
+
+        final List<GameRound> rounds = gameRoundRepository.findByPlayerId(player.playerId());
+        return new GameRoundListing(rounds);
+    }
+
+}
