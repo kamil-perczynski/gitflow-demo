9bdce170417fe6b0b3f04d07a1f723a19a820eaa
Kamil Perczyński <43953005+kamil-perczynski@users.noreply.github.com>
2023-03-17 10:47:09 +0100

    Release 2023 03 17 (#11)
    * Feature a (#7)
    
    * Implement endpoint for reading player by id
    
    * Add logging for reading player by id
    
    ---------
    
    Co-authored-by: Kamil Perczynski <Kamil.Perczynski@raiffeisen.al>
    
    * Feature b (#8)
    
    * Add endpoint for reading game rounds
    
    * Add logging for endpoint for reading game rounds
    
    ---------
    
    Co-authored-by: Kamil Perczynski <Kamil.Perczynski@raiffeisen.al>
    
    * Feature c (#9)
    
    * Add an endpoint for reading game round by id
    
    * Add logging to an endpoint for reading game round by id
    
    ---------
    
    Co-authored-by: Kamil Perczynski <Kamil.Perczynski@raiffeisen.al>
    
    * Revert "Feature a (#7)" (#10)
    
    This reverts commit a30410ee
    
    Co-authored-by: Kamil Perczynski <Kamil.Perczynski@raiffeisen.al>
    
    ---------
    
    Co-authored-by: Kamil Perczynski <Kamil.Perczynski@raiffeisen.al>

--- src/main/java/perczynski/kamil/evolution/gameservice/api/GameController.java
+++ src/main/java/perczynski/kamil/evolution/gameservice/api/GameController.java
@@ -1,19 +1,20 @@
 package perczynski.kamil.evolution.gameservice.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 import perczynski.kamil.evolution.gameservice.domain.GameService;
 import perczynski.kamil.evolution.gameservice.domain.Player;
 import perczynski.kamil.evolution.gameservice.domain.PlayerRepository;
 import perczynski.kamil.evolution.gameservice.domain.bets.Bet;
 import perczynski.kamil.evolution.gameservice.domain.bets.GameRound;
+import perczynski.kamil.evolution.gameservice.domain.rounds.GameRoundListing;
+import perczynski.kamil.evolution.gameservice.domain.rounds.GameRoundService;
 import perczynski.kamil.evolution.gameservice.libs.Money;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @SuppressWarnings("unused")
@@ -21,6 +22,7 @@ public class GameController {
 
     private final GameService gameService;
     private final PlayerRepository playerRepository;
+    private final GameRoundService gameRoundService;
 
     @PostMapping("/players")
     public Player registerPlayer() {
@@ -37,6 +39,18 @@ public class GameController {
                         .stake(new Money(placeBetRequest.stake()))
                         .build()
         );
+    }
+
+    @GetMapping("/game-rounds/{roundId}")
+    public GameRound readGameRound(@PathVariable String roundId) {
+        log.debug("Reading game round by id: {}", roundId);
+        return gameRoundService.readGameRound(roundId);
+    }
+
+    @GetMapping("/game-rounds")
+    public GameRoundListing listRounds(@RequestParam String playerId) {
+        log.debug("Listing game rounds for player: {}", playerId);
+        return gameRoundService.listGameRounds(playerId);
     }
 
 }
