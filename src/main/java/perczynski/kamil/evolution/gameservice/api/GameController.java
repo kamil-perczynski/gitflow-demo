6b52c31f48a566613876585ae0cb546d9302f9e2
Kamil Perczyński <43953005+kamil-perczynski@users.noreply.github.com>
2023-03-17 10:41:19 +0100

    Feature b (#8)
    * Add endpoint for reading game rounds
    
    * Add logging for endpoint for reading game rounds
    
    ---------
    
    Co-authored-by: Kamil Perczynski <Kamil.Perczynski@raiffeisen.al>

--- src/main/java/perczynski/kamil/evolution/gameservice/api/GameController.java
+++ src/main/java/perczynski/kamil/evolution/gameservice/api/GameController.java
@@ -8,6 +8,8 @@ import perczynski.kamil.evolution.gameservice.domain.Player;
 import perczynski.kamil.evolution.gameservice.domain.PlayerRepository;
 import perczynski.kamil.evolution.gameservice.domain.bets.Bet;
 import perczynski.kamil.evolution.gameservice.domain.bets.GameRound;
+import perczynski.kamil.evolution.gameservice.domain.rounds.GameRoundListing;
+import perczynski.kamil.evolution.gameservice.domain.rounds.GameRoundService;
 import perczynski.kamil.evolution.gameservice.libs.Money;
 
 import javax.validation.Valid;
@@ -20,6 +22,7 @@ public class GameController {
 
     private final GameService gameService;
     private final PlayerRepository playerRepository;
+    private final GameRoundService gameRoundService;
 
     @PostMapping("/players")
     public Player registerPlayer() {
@@ -38,6 +41,12 @@ public class GameController {
         );
     }
 
+    @GetMapping("/game-rounds")
+    public GameRoundListing listRounds(@RequestParam String playerId) {
+        log.debug("Listing game rounds for player: {}", playerId);
+        return gameRoundService.listGameRounds(playerId);
+    }
+    
     @GetMapping("/players/{playerId}")
     public Player readPlayer(@PathVariable String playerId) {
         log.debug("Reading player with id: {}", playerId);
